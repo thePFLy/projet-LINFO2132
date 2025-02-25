16b2095a333d450ac32f2e14c4cdd0e8f7476645
Filip De Roover <filip.deroover@student.uclouvain.be>
2025-02-25 13:38:58 +0100

    ajout enum symbols

--- src/main/java/compiler/Lexer/Lexer.java
+++ src/main/java/compiler/Lexer/Lexer.java
@@ -2,8 +2,19 @@ package compiler.Lexer;
 import java.io.Reader;
 
 public class Lexer {
-    
+    public enum Symbols{
+        COMMENT,
+        IDENTIFIER,
+        KEYWORD,
+        INTEGER,
+        FLOAT,
+        STRING,
+        BOOLEAN,
+        SYMBOL
+    }
+    private Reader reader;
     public Lexer(Reader input) {
+        this.reader = input;
     }
     
     public Symbol getNextSymbol() {
