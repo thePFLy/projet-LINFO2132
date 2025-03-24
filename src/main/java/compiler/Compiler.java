25dd924db8c1b14c6838a34430c3aa18e701cd24
PFLY <HE202071@students.ephec.be>
2025-03-25 00:00:39 +0100

    compiler

--- src/main/java/compiler/Compiler.java
+++ src/main/java/compiler/Compiler.java
@@ -2,26 +2,40 @@ package compiler;
 
 import compiler.Lexer.Lexer;
 import compiler.Lexer.Symbol;
+import compiler.Parser.Parser;
+import compiler.Parser.AST.ProgramNode;
 import java.io.FileReader;
 import java.io.Reader;
-import java.io.IOException;
 
 public class Compiler {
-    public static void main(String[] args) throws IOException {
-        if (args.length != 2 || !args[0].equals("-lexer")) {
-            System.out.println("need: -lexer <file>");
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: -mode <file>");
+            System.out.println("Modes: -lexer, -parser");
             return;
         }
+
+        String mode = args[0];
         String file = args[1];
-        // file reading
-        Reader reader = new FileReader(file);
-        // lexer file
-        Lexer lexer = new Lexer(reader);
-        Symbol symbol;
-        // print tokens
-        while ((symbol = lexer.getNextSymbol()).getType() != Lexer.SymbolType.EOF) {
-            System.out.println("token: " + symbol.getType() + " value: " + symbol.getName());
+
+        try (Reader reader = new FileReader(file)) {
+            Lexer lexer = new Lexer(reader);
+
+            if (mode.equals("-lexer")) {
+                Symbol symbol;
+                while ((symbol = lexer.getNextSymbol()).getType() != Lexer.SymbolType.EOF) {
+                    System.out.println("Token: " + symbol.getType() + " \tValue: " + symbol.getName());
+                }
+            } else if (mode.equals("-parser")) {
+                Parser parser = new Parser(lexer);
+                ProgramNode ast = parser.getAST();
+                System.out.println("AST contains " + ast.statements.size() + " top-level statements");
+            } else {
+                System.out.println("Invalid mode. Use -lexer or -parser");
+            }
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
-        reader.close();
     }
 }
